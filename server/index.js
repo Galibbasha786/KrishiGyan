ef1a73ece70ab648edc3354c144885c23676ecd7
Syed Galib Basha <syedsunnygalibbasha@gmail.com>
2026-02-14 12:20:19 +0530

    Update server/index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,11 @@ import passwordResetRoutes from "./routes/passwordResetRoutes.js"
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,    //important to set origin to the client URL 
+  credentials: true,     
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+}));
 app.use(express.json({ limit: "10mb" }));
 
 // API Routes
@@ -59,7 +63,7 @@ app.get("/api/health", (req, res) => {
 });
 
 // 🔥 Connect to MongoDB (env or local fallback)
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/krishigyan";
+const MONGO_URI = process.env.MONGO_URI //|| "mongodb://localhost:27017/krishigyan";
 mongoose
   .connect(MONGO_URI)
   .then(() =>
